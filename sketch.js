27bff52f204b2039098aabf0c2ef2367503bab6d
Nicollybolgioni34 <bolgionin@gmail.com>
2024-09-04 08:53:50 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,37 @@
+line(x1, y1, x2, y2)
+
+function setup() {
+
+createCanvas(200, 200);
+
+background("white");
+
+}
+
+function draw() {
+
+stroke("red");
+
+line(50, 100, 150, 100);
+
+}
+square(x, y, s);
+
+triangle(x1, y1, x2, y2, x3, y3);
+
+function setup() {
+
+createCanvas(200, 200);
+
+background("white");
+
+}
+
+function draw() {
+
+stroke("orange");
+
+triangle(50, 150, 100, 50, 150, 150);
+
+}
+
